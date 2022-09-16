3476384d5fd86b3d2f393eec50905984700bfbf1
SchwatteDuevel <102264881+SchwatteDuevel@users.noreply.github.com>
2022-09-17 00:43:31 +0200

    Initialisation

---
+++ script.js
@@ -0,0 +1,59 @@
+let maxPoints = document.getElementById("maxPoints");
+let achievedPoints = document.getElementById("achievedPoints");
+let resultBtn = document.getElementById("result-btn");
+let resultPercent = document.getElementById("result-percent");
+let resultNote = document.getElementById("result-note");
+let note = "";
+let emoji = "";
+
+resultBtn.addEventListener("click", function() {
+    let percentResult = Math.round((100*achievedPoints.value)/maxPoints.value);
+    resultPercent.innerText = percentResult + "%";  ;
+    if(percentResult <= 29) {
+        note = "6";
+        emoji = "😵";
+    } else if(percentResult <= 34) {
+        note = "5-"
+        emoji = "🥵";
+    } else if(percentResult <= 39) {
+        note = "5"
+        emoji = "🤯";
+    } else if(percentResult <= 44) {
+        note = "5+"
+        emoji = "🙁";
+    } else if(percentResult <= 50) {
+        note = "4-"
+        emoji = "🥺";
+    } else if(percentResult <= 56) {
+        note = "4"
+        emoji = "🤫";
+    } else if(percentResult <= 60) {
+        note = "4+"
+        emoji = "😐";
+    } else if(percentResult <= 65) {
+        note = "3-"
+        emoji = "🤭";
+    } else if(percentResult <= 70) {
+        note = "3"
+        emoji = "🥱";
+    } else if(percentResult <= 75) {
+        note = "3+"
+        emoji = "😏";
+    } else if(percentResult <= 80) {
+        note = "2-"
+        emoji = "😌";
+    } else if(percentResult <= 86) {
+        note = "2"
+        emoji = "😊";
+    } else if(percentResult <= 91) {
+        note = "2+"
+        emoji = "🤓";
+    } else if(percentResult <= 95) {
+        note = "1-"
+        emoji = "🥳";
+    } else {
+        note = "1"
+        emoji = "🤩";
+    }
+       resultNote.innerText = note + " " + emoji;
+})
